59ce896177eaf28c06090e2427c1dda5471f1354
manylovv <yellowmanylov@gmail.com>
2023-03-16 15:43:18 +0700

    feat: create Stable page

---
+++ src/components/pages/Stable.tsx
@@ -0,0 +1,54 @@
+import { useRef, useState } from 'react';
+
+type StableProps = {
+  onBack: () => void;
+  onTransaction: (value: number, action: string) => void;
+};
+
+export default function Stable({ onBack, onTransaction }: StableProps) {
+  const [active, setActive] = useState<'borrow' | 'repay'>('borrow');
+  const button = 'w-full rounded-lg py-2 font-medium text-gray-600';
+  const activeButton = `${button} bg-white`;
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  return (
+    <div className="grid h-screen place-content-center">
+      <div className="container w-screen max-w-xs rounded-xl p-7 shadow">
+        <div className="flex flex-col gap-2">
+          <button type="button" onClick={onBack} className="self-start">
+            ← Go back
+          </button>
+          <div className="flex rounded-xl bg-gray-200 p-1">
+            <button
+              type="button"
+              onClick={() => active !== 'borrow' && setActive('borrow')}
+              className={active === 'borrow' ? activeButton : button}
+            >
+              Borrow
+            </button>
+            <button
+              type="button"
+              onClick={() => active !== 'repay' && setActive('repay')}
+              className={active === 'repay' ? activeButton : button}
+            >
+              Repay
+            </button>
+          </div>
+          <input
+            ref={inputRef}
+            className="w-full rounded-xl border border-gray-200 py-6 px-4 text-2xl font-medium shadow-sm outline-none focus:border-gray-400"
+            placeholder="0.0"
+          />
+          <button
+            onClick={() =>
+              inputRef.current && onTransaction(+inputRef.current.value, active)
+            }
+            className="w-full rounded-xl bg-gray-600 py-3 font-medium text-white"
+          >
+            {active === 'borrow' ? 'Borrow' : 'Repay'}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
