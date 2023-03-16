451b548c009bae4e39a21247dcad956648e4b2c7
manylovv <yellowmanylov@gmail.com>
2023-03-16 15:44:27 +0700

    feat: export pages from one file

---
+++ src/components/pages/index.ts
@@ -0,0 +1,4 @@
+export { default as Home } from './Home';
+export { default as Connect } from './Connect';
+export { default as Collateral } from './Collateral';
+export { default as Stable } from './Stable';
